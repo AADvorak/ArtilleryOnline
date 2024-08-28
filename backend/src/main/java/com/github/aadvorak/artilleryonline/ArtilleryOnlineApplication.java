d9fd9ac5bfa1147c64d7adfebd923107641aca7e
A. Dvorak <dvorak88@mail.ru>
2024-08-28 10:44:30 +0400

    feat: init spring boot project

---
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/ArtilleryOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.github.aadvorak.artilleryonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtilleryOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtilleryOnlineApplication.class, args);
+	}
+
+}
