d2f5bead380e2faa5bced6ed9cd8662daf51bd05
A. Dvorak <dvorak88@mail.ru>
2024-09-27 19:13:20 +0400

    feat: debug switching vehicle

--- playground/src/components/BattleDebugButtons.vue
+++ playground/src/components/BattleDebugButtons.vue
@@ -4,12 +4,14 @@ import { useBattleStore } from '@/stores/battle'
 import { computed } from 'vue'
 import { useCommandsSender } from '@/composables/commands-sender'
 import { Command } from '@/data/command'
+import {useUserStore} from "@/stores/user";
 
 const props = defineProps<{
   stompClient: StompClient
 }>()
 
 const battleStore = useBattleStore()
+const userStore = useUserStore()
 const commandsSender = useCommandsSender(props.stompClient)
 
 const paused = computed(() => {
@@ -27,9 +29,17 @@ function resume() {
 function step() {
   commandsSender.sendDebugCommand({ command: Command.STEP })
 }
+
+function switchVehicle() {
+  const otherUserKey = Object.keys(battleStore.vehicles).filter(key => key !== userStore.userKey)[0]
+  if (otherUserKey) {
+    userStore.userKey = otherUserKey
+  }
+}
 </script>
 
 <template>
+  <v-btn color="primary" @click="switchVehicle">Switch Vehicle</v-btn>
   <v-btn v-if="!paused" color="warning" @click="pause">Pause</v-btn>
   <v-btn v-if="paused" color="success" @click="resume">Resume</v-btn>
   <v-btn v-if="paused" color="warning" @click="step">Step</v-btn>
