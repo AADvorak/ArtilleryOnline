5caa786d8918add02c4ea9f33f645d65a3b8d261
A. Dvorak <dvorak88@mail.ru>
2024-10-01 11:35:24 +0400

    fix: VehicleGroundCollideProcessor improvements

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/VehicleGroundCollideProcessor.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/VehicleGroundCollideProcessor.java
@@ -10,16 +10,18 @@ public class VehicleGroundCollideProcessor {
 
     public static boolean processCollide(VehicleCalculations calculations, VehicleModel vehicleModel,
                                          BattleModel battleModel) {
-        if (groundCollide(calculations, vehicleModel, battleModel)) {
-            doCollide(vehicleModel);
+        var groundCollideAngle = getGroundCollideAngle(calculations, vehicleModel, battleModel);
+        if (groundCollideAngle != null) {
+            doCollide(vehicleModel, groundCollideAngle);
+            vehicleModel.setCollided(true);
             battleModel.setUpdated(true);
             return true;
         }
         return false;
     }
 
-    private static boolean groundCollide(VehicleCalculations calculations, VehicleModel vehicleModel,
-                                         BattleModel battleModel) {
+    private static Double getGroundCollideAngle(VehicleCalculations calculations, VehicleModel vehicleModel,
+                                                BattleModel battleModel) {
         var nextRightWheelPosition = VehicleUtils.getRightWheelPosition(vehicleModel, calculations.getNextPosition(),
                 calculations.getNextAngle());
         var nextLeftWheelPosition = VehicleUtils.getLeftWheelPosition(vehicleModel, calculations.getNextPosition(),
@@ -28,18 +30,28 @@ public class VehicleGroundCollideProcessor {
                 battleModel.getRoom());
         var leftWheelNearestGroundPoint = BattleUtils.getNearestGroundPosition(nextLeftWheelPosition.getX(),
                 battleModel.getRoom());
-        return rightWheelNearestGroundPoint.getY() >= nextRightWheelPosition.getY()
-                || leftWheelNearestGroundPoint.getY() >= nextLeftWheelPosition.getY();
+        if (rightWheelNearestGroundPoint.getY() >= nextRightWheelPosition.getY()
+                && calculations.getRightWheel().getNearestGroundPointByX().getY()
+                < calculations.getRightWheel().getPosition().getY()) {
+            return calculations.getRightWheel().getGroundAngle();
+        }
+        if (leftWheelNearestGroundPoint.getY() >= nextLeftWheelPosition.getY()
+                && calculations.getLeftWheel().getNearestGroundPointByX().getY()
+                < calculations.getLeftWheel().getPosition().getY() ) {
+            return calculations.getLeftWheel().getGroundAngle();
+        }
+        return null;
     }
 
-    private static void doCollide(VehicleModel vehicleModel) {
-        vehicleModel.getState().getVehicleVelocity().setX(
-                - vehicleModel.getState().getVehicleVelocity().getX() / 2);
-        vehicleModel.getState().getVehicleVelocity().setAngle(
-                vehicleModel.getState().getVehicleVelocity().getAngle() / 2);
-        if (vehicleModel.getState().getVehicleVelocity().getY() < 0) {
-            vehicleModel.getState().getVehicleVelocity().setY(
-                    - vehicleModel.getState().getVehicleVelocity().getY() / 2);
-        }
+    private static void doCollide(VehicleModel vehicleModel, double angle) {
+        var vehicleVelocity = vehicleModel.getState().getVehicleVelocity();
+        var velocityVerticalProjection = vehicleVelocity.getX() * Math.sin(angle) + vehicleVelocity.getY() * Math.cos(angle);
+        var velocityHorizontalProjection = vehicleVelocity.getX() * Math.cos(angle) + vehicleVelocity.getY() * Math.sin(angle);
+        velocityVerticalProjection = - 2 * velocityVerticalProjection;
+        vehicleVelocity.setX(velocityHorizontalProjection * Math.cos(angle)
+                + velocityVerticalProjection * Math.sin(angle));
+        vehicleVelocity.setY(velocityHorizontalProjection * Math.sin(angle)
+                + velocityVerticalProjection * Math.cos(angle));
+        vehicleVelocity.setAngle(vehicleVelocity.getAngle() / 2);
     }
 }
