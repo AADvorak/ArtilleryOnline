8ebe6fa1dd130d158274d21da07c908677aa726c
A. Dvorak <dvorak88@mail.ru>
2024-10-08 21:56:11 +0400

    feat: menu frontend init module

---
+++ menu/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-app full-height theme="dark">
+    <div class="d-flex align-center justify-center flex-column default">
+      <slot />
+    </div>
+  </v-app>
+</template>
+
+<style scoped>
+.default {
+  height: 100%;
+}
+</style>
---
+++ menu/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-card width="100%" max-width="600px">
+    <v-card-title>
+      Artillery online
+    </v-card-title>
+    <v-card-text>
+      <v-btn class="mb-4" width="100%" color="primary">Sign in</v-btn>
+      <v-btn class="mb-4" width="100%" color="secondary">Sign up</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
