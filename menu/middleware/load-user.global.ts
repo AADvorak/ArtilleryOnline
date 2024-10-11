275924a6cdcb3e442ede08c7821014f6947d96a0
A. Dvorak <dvorak88@mail.ru>
2024-10-11 10:06:52 +0400

    fix, refactor: move load setting and load battle to middleware

--- menu/middleware/load-user.global.ts
+++ menu/middleware/load-user.global.ts
@@ -1,20 +1,49 @@
 import {useUserStore} from "~/stores/user";
+import {useSettingsStore} from "~/stores/settings";
+import {useBattleStore} from "~/stores/battle";
 import {ApiRequestSender} from "~/api/api-request-sender";
 import type {User} from "~/data/model";
+import type {ApplicationSettings} from "~/playground/data/common";
+import type {Battle} from "~/playground/data/battle";
 
 const UNSIGNED_PATHS = ['/', '/sign-in', '/sign-up']
 
 export default defineNuxtRouteMiddleware(async (to) => {
+  const api = new ApiRequestSender()
   const userStore = useUserStore()
-  if (!userStore.user) {
+  const settingsStore = useSettingsStore()
+  const battleStore = useBattleStore()
+
+  if (!settingsStore.settings) {
     try {
-      userStore.user = await new ApiRequestSender().getJson<User>('/users/me')
+      settingsStore.settings = await api.getJson<ApplicationSettings>('/application/settings')
     } catch (e) {
       console.log(e)
     }
   }
+
+  if (!userStore.user) {
+    try {
+      userStore.user = await api.getJson<User>('/users/me')
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  if (!!userStore.user && !battleStore.battle) {
+    try {
+      const battle = await api.getJson<Battle>('/battles')
+      battleStore.updateBattle(battle)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   if (!userStore.user && !UNSIGNED_PATHS.includes(to.path)) {
     return navigateTo('/')
+  }
+  if (!!userStore.user && !!battleStore.battle && to.path !== '/playground') {
+    return navigateTo('/playground')
   }
   if (!!userStore.user && UNSIGNED_PATHS.includes(to.path)) {
     return navigateTo('/menu')
--- menu/playground/views/MainView.vue
+++ menu/playground/views/MainView.vue
@@ -3,23 +3,8 @@ import BattleHeader from "@/playground/components/BattleHeader.vue";
 import BattleCanvas from "@/playground/components/BattleCanvas.vue";
 import FinishBattleDialog from "@/playground/components/FinishBattleDialog.vue";
 import {useStompClient} from "@/playground/composables/stomp-client";
-import {useSettingsStore} from "~/stores/settings";
-import {onMounted} from "vue";
-import {ApiRequestSender} from "@/api/api-request-sender";
-import type {ApplicationSettings} from "@/playground/data/common";
 
 const stompClient = useStompClient()
-
-const settingsStore = useSettingsStore()
-
-onMounted(() => {
-  loadApplicationSettings()
-})
-
-async function loadApplicationSettings() {
-  settingsStore.settings = await new ApiRequestSender()
-      .getJson<ApplicationSettings>('/application/settings')
-}
 </script>
 
 <template>
