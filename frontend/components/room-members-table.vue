661553aa110f28a8c17d52319bba025ffaf91240
A. Dvorak <dvorak88@mail.ru>
2024-11-04 21:22:39 +0400

    feat: room members table icons and sorting

--- frontend/components/room-members-table.vue
+++ frontend/components/room-members-table.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-import {useRoomStore} from "~/stores/room";
+import {useRoomStore} from '~/stores/room'
+import {mdiCrown, mdiKnifeMilitary} from '@mdi/js'
+import type {RoomMember} from '~/data/model'
 
 const roomStore = useRoomStore()
 
 const roomMembers = computed(() => {
-  return roomStore.room?.members || []
+  return roomStore.room?.members.sort(sortMembers) || []
 })
+
+function sortMembers(a: RoomMember, b: RoomMember) {
+  if (a.owner) {
+    return -1
+  }
+  return a.nickname > b.nickname ? 1 : -1
+}
 </script>
 
 <template>
@@ -18,16 +27,15 @@ const roomMembers = computed(() => {
       <th class="text-left">
         Selected vehicle
       </th>
-      <th class="text-left">
-        Type
-      </th>
     </tr>
     </thead>
     <tbody>
     <tr v-for="roomMember of roomMembers">
-      <td>{{ roomMember.nickname }}</td>
+      <td>
+        <v-icon :icon="roomMember.owner ? mdiCrown : mdiKnifeMilitary" />
+        {{ roomMember.nickname }}
+      </td>
       <td>{{ roomMember.selectedVehicle }}</td>
-      <td>{{ roomMember.owner ? 'owner' : 'guest' }}</td>
     </tr>
     </tbody>
   </v-table>
