f5b97b0c3314dafc865c5023a16ea7f6622b6eea
A. Dvorak <dvorak88@mail.ru>
2024-12-03 14:46:30 +0400

    fix: date-picker-select format

--- frontend/components/date-picker-select.vue
+++ frontend/components/date-picker-select.vue
@@ -10,6 +10,19 @@ const emit = defineEmits(['select'])
 const menuOpened = ref(false)
 const selectedDate = ref<Date | undefined>()
 
+const selectedDateFormat = computed({
+  get() {
+    return selectedDate.value?.toLocaleDateString()
+  },
+  set(newValue) {
+    if (newValue) {
+      selectedDate.value = new Date(newValue)
+    } else {
+      selectedDate.value = undefined
+    }
+  }
+})
+
 watch(selectedDate, value => {
   menuOpened.value = false
   emit('select', value)
@@ -18,7 +31,7 @@ watch(selectedDate, value => {
 
 <template>
   <v-select
-      v-model="selectedDate"
+      v-model="selectedDateFormat"
       v-model:menu="menuOpened"
       density="compact"
       :label="props.name"
