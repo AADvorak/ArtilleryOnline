cc9cc851823ef950a452147d9dd548739af6e6fd
A. Dvorak <dvorak88@mail.ru>
2025-02-08 13:16:04 +0400

    fix: vehicle-drawer.ts

--- frontend/playground/composables/drawer/vehicle-drawer.ts
+++ frontend/playground/composables/drawer/vehicle-drawer.ts
@@ -9,7 +9,7 @@ import {AppearancesNames} from "~/dictionary/appearances-names";
 
 export function useVehicleDrawer(
     drawerBase: DrawerBase,
-    ctx: Ref<CanvasRenderingContext2D>
+    ctx: Ref<CanvasRenderingContext2D | undefined>
 ) {
   const battleStore = useBattleStore()
   const userSettingsStore = useUserSettingsStore()
@@ -50,42 +50,42 @@ export function useVehicleDrawer(
   function drawHull(vehicleModel: VehicleModel, position: Position, radius: number) {
     const startAngle = Math.PI - vehicleModel.state.position.angle
     const endAngle = 2 * Math.PI - vehicleModel.state.position.angle
-    ctx.value.beginPath()
-    ctx.value.arc(position.x, position.y, radius, startAngle, endAngle)
-    ctx.value.fill()
-    ctx.value.closePath()
+    ctx.value!.beginPath()
+    ctx.value!.arc(position.x, position.y, radius, startAngle, endAngle)
+    ctx.value!.fill()
+    ctx.value!.closePath()
   }
 
   function drawWheels(vehicleModel: VehicleModel, wheelRadius: number) {
-    ctx.value.beginPath()
+    ctx.value!.beginPath()
     const rightWheelPosition = drawerBase.transformPosition(
         VehicleUtils.getRightWheelPosition(vehicleModel)
     )
     const leftWheelPosition = drawerBase.transformPosition(
         VehicleUtils.getLeftWheelPosition(vehicleModel)
     )
-    ctx.value.arc(rightWheelPosition.x, rightWheelPosition.y, wheelRadius, 0, 2 * Math.PI)
-    ctx.value.fill()
-    ctx.value.arc(leftWheelPosition.x, leftWheelPosition.y, wheelRadius, 0, 2 * Math.PI)
-    ctx.value.fill()
-    ctx.value.closePath()
+    ctx.value!.arc(rightWheelPosition.x, rightWheelPosition.y, wheelRadius, 0, 2 * Math.PI)
+    ctx.value!.fill()
+    ctx.value!.arc(leftWheelPosition.x, leftWheelPosition.y, wheelRadius, 0, 2 * Math.PI)
+    ctx.value!.fill()
+    ctx.value!.closePath()
   }
 
   function drawSmallWheels(vehicleModel: VehicleModel, wheelRadius: number) {
     VehicleUtils.getSmallWheels(vehicleModel)
         .map(drawerBase.transformPosition)
         .forEach((position) => {
-          ctx.value.beginPath()
-          ctx.value.arc(position.x, position.y, wheelRadius / 2, 0, 2 * Math.PI)
-          ctx.value.fill()
-          ctx.value.closePath()
+          ctx.value!.beginPath()
+          ctx.value!.arc(position.x, position.y, wheelRadius / 2, 0, 2 * Math.PI)
+          ctx.value!.fill()
+          ctx.value!.closePath()
         })
   }
 
   function drawTrack(vehicleModel: VehicleModel) {
     if (!vehicleModel.state.trackState.broken) {
-      ctx.value.beginPath()
-      ctx.value.lineWidth = 2
+      ctx.value!.beginPath()
+      ctx.value!.lineWidth = 2
       const bottomTrackBeginPosition = drawerBase.transformPosition(
           VehicleUtils.getLeftWheelBottomPosition(vehicleModel)
       )
@@ -98,13 +98,13 @@ export function useVehicleDrawer(
       const topTrackEndPosition = drawerBase.transformPosition(
           VehicleUtils.getRightWheelTopPosition(vehicleModel)
       )
-      ctx.value.moveTo(bottomTrackBeginPosition.x, bottomTrackBeginPosition.y)
-      ctx.value.lineTo(bottomTrackEndPosition.x, bottomTrackEndPosition.y)
-      ctx.value.stroke()
-      ctx.value.moveTo(topTrackBeginPosition.x, topTrackBeginPosition.y)
-      ctx.value.lineTo(topTrackEndPosition.x, topTrackEndPosition.y)
-      ctx.value.stroke()
-      ctx.value.closePath()
+      ctx.value!.moveTo(bottomTrackBeginPosition.x, bottomTrackBeginPosition.y)
+      ctx.value!.lineTo(bottomTrackEndPosition.x, bottomTrackEndPosition.y)
+      ctx.value!.stroke()
+      ctx.value!.moveTo(topTrackBeginPosition.x, topTrackBeginPosition.y)
+      ctx.value!.lineTo(topTrackEndPosition.x, topTrackEndPosition.y)
+      ctx.value!.stroke()
+      ctx.value!.closePath()
     }
   }
 
@@ -112,42 +112,42 @@ export function useVehicleDrawer(
     const gunEndPosition = drawerBase.transformPosition(
         VehicleUtils.getGunEndPosition(vehicleModel)
     )
-    ctx.value.beginPath()
-    ctx.value.lineWidth = drawerBase.scale(vehicleModel.config.gun.caliber)
-    ctx.value.moveTo(position.x, position.y)
-    ctx.value.lineTo(gunEndPosition.x, gunEndPosition.y)
-    ctx.value.stroke()
-    ctx.value.closePath()
+    ctx.value!.beginPath()
+    ctx.value!.lineWidth = drawerBase.scale(vehicleModel.config.gun.caliber)
+    ctx.value!.moveTo(position.x, position.y)
+    ctx.value!.lineTo(gunEndPosition.x, gunEndPosition.y)
+    ctx.value!.stroke()
+    ctx.value!.closePath()
   }
 
   function drawHpBar(vehicleModel: VehicleModel) {
-    ctx.value.beginPath()
-    ctx.value.lineWidth = 1
+    ctx.value!.beginPath()
+    ctx.value!.lineWidth = 1
     const hpBarHeight = 0.07
     const hpBarTopLeft = drawerBase.transformPosition({
       x: vehicleModel.state.position.x - vehicleModel.specs.radius,
       y: vehicleModel.state.position.y + 2 * vehicleModel.specs.radius + hpBarHeight
     })
     const hpRatio = vehicleModel.state.hitPoints / vehicleModel.specs.hitPoints
-    ctx.value.strokeRect(hpBarTopLeft.x, hpBarTopLeft.y,
+    ctx.value!.strokeRect(hpBarTopLeft.x, hpBarTopLeft.y,
         drawerBase.scale(2 * vehicleModel.specs.radius), drawerBase.scale(hpBarHeight))
-    ctx.value.fillRect(hpBarTopLeft.x, hpBarTopLeft.y,
+    ctx.value!.fillRect(hpBarTopLeft.x, hpBarTopLeft.y,
         drawerBase.scale(2 * vehicleModel.specs.radius * hpRatio), drawerBase.scale(hpBarHeight))
-    ctx.value.closePath()
+    ctx.value!.closePath()
   }
 
   function drawNickname(userKey: string, vehicleModel: VehicleModel) {
-    ctx.value.beginPath()
-    ctx.value.fillStyle = 'rgb(256,256,256)'
-    ctx.value.font = '16px arial'
-    ctx.value.lineWidth = 1
+    ctx.value!.beginPath()
+    ctx.value!.fillStyle = 'rgb(256,256,256)'
+    ctx.value!.font = '16px arial'
+    ctx.value!.lineWidth = 1
     const nicknamePosition = drawerBase.transformPosition({
       x: vehicleModel.state.position.x - vehicleModel.specs.radius,
       y: vehicleModel.state.position.y + 2 * vehicleModel.specs.radius + 0.1
     })
-    ctx.value.fillText(restrictNicknameLength(userKey), nicknamePosition.x, nicknamePosition.y,
+    ctx.value!.fillText(restrictNicknameLength(userKey), nicknamePosition.x, nicknamePosition.y,
         drawerBase.scale(2 * vehicleModel.specs.radius))
-    ctx.value.closePath()
+    ctx.value!.closePath()
   }
 
   function restrictNicknameLength(nickname: string) {
