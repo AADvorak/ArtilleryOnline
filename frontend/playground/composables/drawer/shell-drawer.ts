b4ed91aa1a135a5358b73c55ad95c4569255b99a
A. Dvorak <dvorak88@mail.ru>
2025-02-19 16:26:20 +0400

    feat: shell-drawer change shape

--- frontend/playground/composables/drawer/shell-drawer.ts
+++ frontend/playground/composables/drawer/shell-drawer.ts
@@ -2,6 +2,8 @@ import type { DrawerBase } from '@/playground/composables/drawer/drawer-base'
 import type { Ref } from 'vue'
 import { useBattleStore } from '~/stores/battle'
 import type { ShellModel } from '@/playground/data/model'
+import {VectorUtils} from "~/playground/utils/vector-utils";
+import {BattleUtils} from "~/playground/utils/battle-utils";
 
 export function useShellDrawer(
   drawerBase: DrawerBase,
@@ -17,12 +19,20 @@ export function useShellDrawer(
 
   function drawShell(shellModel: ShellModel) {
     if (ctx.value) {
+      const caliber = shellModel.specs.caliber
+      const rawHead = shellModel.state.position
+      const angle = VectorUtils.getAngle(shellModel.state.velocity)
+      const rawTail = BattleUtils.shiftedPosition(rawHead, - 3 * caliber, angle)
+      const head = drawerBase.transformPosition(rawHead)
+      const tailRight = drawerBase.transformPosition(BattleUtils.shiftedPosition(rawTail, caliber, angle + Math.PI / 2))
+      const tailLeft = drawerBase.transformPosition(BattleUtils.shiftedPosition(rawTail, caliber, angle - Math.PI / 2))
+
       ctx.value.fillStyle = 'rgb(256 256 256)'
       ctx.value.lineWidth = 1
       ctx.value.beginPath()
-      const radius = drawerBase.scale(shellModel.specs.caliber / 2)
-      const position = drawerBase.transformPosition(shellModel.state.position)
-      ctx.value.arc(position.x, position.y, radius, 0, 2 * Math.PI)
+      ctx.value.moveTo(head.x, head.y)
+      ctx.value.lineTo(tailRight.x, tailRight.y)
+      ctx.value.lineTo(tailLeft.x, tailLeft.y)
       ctx.value.fill()
       ctx.value.closePath()
     }
