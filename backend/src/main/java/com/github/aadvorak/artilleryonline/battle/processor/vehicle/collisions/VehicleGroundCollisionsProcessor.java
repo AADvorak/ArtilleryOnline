f6996cbc8a694b1db58c90a0c88f94475f853215
A. Dvorak <dvorak88@mail.ru>
2025-02-23 13:23:17 +0400

    fix: VehicleGroundCollisionsProcessor redundant code

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/collisions/VehicleGroundCollisionsProcessor.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/collisions/VehicleGroundCollisionsProcessor.java
@@ -2,13 +2,11 @@ package com.github.aadvorak.artilleryonline.battle.processor.vehicle.collisions;
 
 import com.github.aadvorak.artilleryonline.battle.calculations.BattleCalculations;
 import com.github.aadvorak.artilleryonline.battle.calculations.VehicleCalculations;
-import com.github.aadvorak.artilleryonline.battle.utils.BattleUtils;
 import com.github.aadvorak.artilleryonline.battle.utils.CollisionUtils;
 
 public class VehicleGroundCollisionsProcessor {
 
     public static void process(VehicleCalculations vehicle, BattleCalculations battle) {
-        calculateNextGroundPositions(vehicle, battle);
         var collision = VehicleGroundCollisionsDetector.detectFirst(vehicle, battle);
         if (collision != null) {
             CollisionUtils.resolveGroundCollision(collision, battle);
@@ -19,20 +17,7 @@ public class VehicleGroundCollisionsProcessor {
     }
 
     public static boolean checkResolved(VehicleCalculations vehicle, BattleCalculations battle) {
-        calculateNextGroundPositions(vehicle, battle);
         var collision = VehicleGroundCollisionsDetector.detectFirst(vehicle, battle);
         return collision == null;
-    }
-
-    // todo is it needed?
-    private static void calculateNextGroundPositions(VehicleCalculations vehicle, BattleCalculations battle) {
-        vehicle.getRightWheel().getNext().setNearestGroundPointByX(BattleUtils.getNearestGroundPosition(
-                vehicle.getRightWheel().getNext().getPosition().getX(),
-                battle.getModel().getRoom()
-        ));
-        vehicle.getLeftWheel().getNext().setNearestGroundPointByX(BattleUtils.getNearestGroundPosition(
-                vehicle.getLeftWheel().getNext().getPosition().getX(),
-                battle.getModel().getRoom()
-        ));
     }
 }
