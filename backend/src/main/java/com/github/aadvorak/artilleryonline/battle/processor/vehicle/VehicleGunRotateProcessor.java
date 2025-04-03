9fe2c02844bbb912cde3e35397c808fa119ad662
A. Dvorak <dvorak88@mail.ru>
2025-04-03 11:17:15 +0400

    fix: vehicle gun rotate processor two modes

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/VehicleGunRotateProcessor.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/VehicleGunRotateProcessor.java
@@ -8,25 +8,32 @@ import com.github.aadvorak.artilleryonline.battle.model.VehicleModel;
 public class VehicleGunRotateProcessor {
 
     public static void processStep(VehicleModel vehicleModel, BattleModel battleModel) {
-        var rotatingDirection = vehicleModel.getState().getGunState().getRotatingDirection();
+        var gunState = vehicleModel.getState().getGunState();
+        var rotatingDirection = gunState.getRotatingDirection();
         var rotatingVelocity = vehicleModel.getConfig().getGun().getRotationVelocity();
         var maxGunAngle = vehicleModel.getSpecs().getMaxAngle();
         var minGunAngle = vehicleModel.getSpecs().getMinAngle();
         var vehicleAngle = vehicleModel.getState().getPosition().getAngle();
-        var targetAngle = vehicleModel.getState().getGunState().getTargetAngle();
+        var targetAngle = gunState.isFixed()
+                ? vehicleAngle + gunState.getAngle()
+                : gunState.getTargetAngle();
         if (rotatingDirection != null) {
             targetAngle += (MovingDirection.RIGHT.equals(rotatingDirection) ? -1 : 1)
                     * battleModel.getCurrentTimeStepSecs() * rotatingVelocity;
         }
         targetAngle = restrictValue(targetAngle, minGunAngle + vehicleAngle, maxGunAngle + vehicleAngle);
-        vehicleModel.getState().getGunState().setTargetAngle(targetAngle);
-        var gunAngle = vehicleModel.getState().getGunState().getAngle();
-        var angleDiff = targetAngle - vehicleAngle - gunAngle;
-        if (Math.abs(angleDiff) > rotatingVelocity * Battle.TIME_STEP_MS / 1000) {
-            var angleStep = Math.signum(angleDiff) * rotatingVelocity * battleModel.getCurrentTimeStepSecs();
-            gunAngle += Math.min(angleStep, angleDiff);
-            gunAngle = restrictValue(gunAngle, minGunAngle, maxGunAngle);
-            vehicleModel.getState().getGunState().setAngle(gunAngle);
+        gunState.setTargetAngle(targetAngle);
+        if (gunState.isFixed()) {
+            gunState.setAngle(targetAngle - vehicleAngle);
+        } else {
+            var gunAngle = gunState.getAngle();
+            var angleDiff = targetAngle - vehicleAngle - gunAngle;
+            if (Math.abs(angleDiff) > rotatingVelocity * Battle.TIME_STEP_MS / 1000) {
+                var angleStep = Math.signum(angleDiff) * rotatingVelocity * battleModel.getCurrentTimeStepSecs();
+                gunAngle += Math.min(angleStep, angleDiff);
+                gunAngle = restrictValue(gunAngle, minGunAngle, maxGunAngle);
+                gunState.setAngle(gunAngle);
+            }
         }
     }
 
