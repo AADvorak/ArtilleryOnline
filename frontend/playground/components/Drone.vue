1b22bc094f481488a37bd446d1f35e83143324b4
A. Dvorak <dvorak88@mail.ru>
2025-04-04 14:08:04 +0400

    feat: launch drone by button click

--- frontend/playground/components/Drone.vue
+++ frontend/playground/components/Drone.vue
@@ -3,8 +3,12 @@ import { computed } from 'vue'
 import { useBattleStore } from '~/stores/battle'
 import {useUserStore} from '~/stores/user'
 import {useI18n} from "vue-i18n";
+import {useCommandsSender} from "~/playground/composables/commands-sender";
+import {Command} from "~/playground/data/command";
 
 const {t} = useI18n()
+
+const commandsSender = useCommandsSender()
 
 const userStore = useUserStore()
 const battleStore = useBattleStore()
@@ -16,14 +20,24 @@ const userVehicle = computed(() => {
 const droneState = computed(() => {
   return userVehicle.value?.state.droneState
 })
+
+function launch() {
+  commandsSender.sendCommand({
+    command: Command.LAUNCH_DRONE
+  })
+}
 </script>
 
 <template>
-  <v-btn v-if="droneState && !droneState.launched"
+  <v-btn
+      v-if="droneState && !droneState.launched"
       class="drone-btn"
       :color="droneState.readyToLaunch ? 'success' : 'warning'"
+      @click="launch"
   >
-    {{ t('battleHeader.drone') }}: {{ droneState.readyToLaunch ? t('battleHeader.ready') : t('battleHeader.preparing') }}
+    {{ t('battleHeader.drone') }}: {{
+      droneState.readyToLaunch ? t('battleHeader.ready') : t('battleHeader.preparing')
+    }}
   </v-btn>
 </template>
 
