8bf15bc85c53d3d620811c54876f64ec195bc308
A. Dvorak <dvorak88@mail.ru>
2025-04-12 16:50:12 +0400

    fix: RegisterRequest limits

--- backend/src/main/java/com/github/aadvorak/artilleryonline/dto/request/RegisterRequest.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/dto/request/RegisterRequest.java
@@ -12,14 +12,14 @@ public class RegisterRequest {
 
     @Email
     @NotEmpty
-    @Size(min = 5, max = 20)
+    @Size(min = 3, max = 50)
     private String email;
 
     @NotEmpty
-    @Size(min = 5, max = 20)
+    @Size(min = 5, max = 50)
     private String password;
 
     @NotEmpty
-    @Size(min = 5, max = 20)
+    @Size(min = 2, max = 20)
     private String nickname;
 }
