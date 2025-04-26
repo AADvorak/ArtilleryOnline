94b9977457ed412829fb1c5945a559c6af3a24f5
A. Dvorak <dvorak88@mail.ru>
2025-04-26 14:30:37 +0400

    feat: body collision

---
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/collision/BodyCollisionData.java
@@ -0,0 +1,88 @@
+package com.github.aadvorak.artilleryonline.battle.collision;
+
+import com.github.aadvorak.artilleryonline.battle.common.*;
+import com.github.aadvorak.artilleryonline.battle.common.lines.Segment;
+import com.github.aadvorak.artilleryonline.battle.model.BodyModel;
+import com.github.aadvorak.artilleryonline.battle.utils.GeometryUtils;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class BodyCollisionData {
+
+    private Velocity velocity;
+
+    private VectorProjections velocityProjections;
+
+    private double inertiaToMassCoefficient;
+
+    private byte rotationSign;
+
+    private double distanceToAxis;
+
+    private double resultMass;
+
+    public static BodyCollisionData of(BodyModel<?, ?, ?, ?> bodyModel, Contact contact) {
+        var velocityAtPosition = getVelocityAtPosition(bodyModel, contact.position());
+        var radiusAndNormalVectorProduct = getRadiusAndNormalVectorProduct(bodyModel, contact);
+        var inertiaToMassCoefficient = getInertiaToMassCoefficient(radiusAndNormalVectorProduct);
+        var distanceToAxis = getDistanceToAxis(bodyModel, contact);
+        return new BodyCollisionData()
+                .setVelocity(velocityAtPosition)
+                .setVelocityProjections(velocityAtPosition.projections(contact.angle()))
+                .setInertiaToMassCoefficient(inertiaToMassCoefficient)
+                .setRotationSign((byte) Math.signum(radiusAndNormalVectorProduct))
+                .setDistanceToAxis(distanceToAxis)
+                .setResultMass(getResultMass(bodyModel, distanceToAxis, inertiaToMassCoefficient));
+    }
+
+    private static Velocity getVelocityAtPosition(BodyModel<?, ?, ?, ?> bodyModel, Position position) {
+        var bodyVelocity = bodyModel.getState().getVelocity();
+        var comPosition = bodyModel.getState().getPosition().getCenter();
+        var angle = comPosition.angleTo(position);
+        var distance = comPosition.distanceTo(position);
+        var rvx = bodyVelocity.getAngle() * distance * Math.sin(angle);
+        var rvy = bodyVelocity.getAngle() * distance * Math.cos(angle);
+        return new Velocity()
+                .setX(bodyVelocity.getX() + rvx)
+                .setY(bodyVelocity.getY() + rvy);
+    }
+
+    private static double getRadiusAndNormalVectorProduct(BodyModel<?, ?, ?, ?> bodyModel, Contact contact) {
+        var comPosition = bodyModel.getState().getPosition().getCenter();
+        var radiusNormalized = comPosition.vectorTo(contact.position()).normalized();
+        return radiusNormalized.vectorProduct(contact.normal());
+    }
+
+    private static double getInertiaToMassCoefficient(double vectorProduct) {
+        var inertiaToMassCoefficient = Math.abs(vectorProduct);
+        if (inertiaToMassCoefficient < 0.1) {
+            return 0;
+        }
+        if (inertiaToMassCoefficient > 0.9) {
+            return 1;
+        }
+        return inertiaToMassCoefficient;
+    }
+
+    private static double getDistanceToAxis(BodyModel<?, ?, ?, ?> bodyModel, Contact contact) {
+        var axis = new Segment(contact.position(), contact.position().shifted(contact.normal()));
+        var comPosition = bodyModel.getState().getPosition().getCenter();
+        var axisProjection = GeometryUtils.getPointToLineProjection(comPosition, axis);
+        return comPosition.distanceTo(axisProjection);
+    }
+
+    private static double getResultMass(BodyModel<?, ?, ?, ?> bodyModel,
+                                        double distanceToAxis, double inertiaToMassCoefficient) {
+        var mass = bodyModel.getPreCalc().getMass();
+        if (inertiaToMassCoefficient == 0.0) {
+            return mass;
+        }
+        var momentOfInertia = bodyModel.getPreCalc().getMomentOfInertia();
+        return inertiaToMassCoefficient * momentOfInertia / Math.pow(distanceToAxis, 2)
+                + (1 - inertiaToMassCoefficient) * mass;
+    }
+}
---
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/collision/CollisionResolver.java
@@ -0,0 +1,74 @@
+package com.github.aadvorak.artilleryonline.battle.collision;
+
+import com.github.aadvorak.artilleryonline.battle.calculations.BodyCalculations;
+import com.github.aadvorak.artilleryonline.battle.common.BodyVelocity;
+import com.github.aadvorak.artilleryonline.battle.common.Collision;
+import com.github.aadvorak.artilleryonline.battle.common.Contact;
+import com.github.aadvorak.artilleryonline.battle.common.VectorProjections;
+import com.github.aadvorak.artilleryonline.battle.model.BodyModel;
+
+public class CollisionResolver {
+
+    private static final double RESTITUTION = 0.5;
+
+    public void resolve(Collision collision) {
+        BodyModel<?,?,?,?> firstModel = null;
+        BodyModel<?,?,?,?> secondModel = null;
+        BodyCollisionData firstData = null;
+        BodyCollisionData secondData = null;
+        if (collision.getPair().first() != null
+                && collision.getPair().first() instanceof BodyCalculations<?,?,?,?,?> bodyCalculations) {
+            firstModel = bodyCalculations.getModel();
+            firstData = BodyCollisionData.of(bodyCalculations.getModel(), collision.getContact());
+        }
+        if (collision.getPair().second() != null
+                && collision.getPair().second() instanceof BodyCalculations<?,?,?,?,?> bodyCalculations) {
+            secondModel = bodyCalculations.getModel();
+            secondData = BodyCollisionData.of(bodyCalculations.getModel(), collision.getContact());
+        }
+        if (firstModel == null) {
+            return;
+        }
+        var closingVelocity = -firstData.getVelocityProjections().getNormal();
+        if (secondData != null) {
+            closingVelocity += secondData.getVelocityProjections().getNormal();
+        }
+        if (closingVelocity <= 0) {
+            return;
+        }
+        if (secondData == null) {
+            // todo
+        } else {
+            var impulseDelta = firstData.getResultMass() * secondData.getResultMass() * closingVelocity
+                    * (1 + RESTITUTION) / (secondData.getResultMass() + firstData.getResultMass());
+            recalculateBodyVelocity(firstModel.getState().getVelocity(), collision.getContact(),
+                    firstData, impulseDelta, 1);
+            recalculateBodyVelocity(secondModel.getState().getVelocity(), collision.getContact(),
+                    secondData, impulseDelta, -1);
+        }
+    }
+
+    private void recalculateBodyVelocity(
+            BodyVelocity velocity,
+            Contact contact,
+            BodyCollisionData collisionData,
+            double impulseDelta,
+            int sign
+    ) {
+        var velocityDelta = sign * impulseDelta / collisionData.getResultMass();
+        if (collisionData.getInertiaToMassCoefficient() > 0) {
+            var angleVelocityDelta = collisionData.getRotationSign() * collisionData.getInertiaToMassCoefficient()
+                    * velocityDelta / collisionData.getDistanceToAxis();
+            velocity.setAngle(velocity.getAngle() + angleVelocityDelta);
+        }
+        if (collisionData.getInertiaToMassCoefficient() < 1) {
+            var movingVelocityDeltaMagnitude = (1 - collisionData.getInertiaToMassCoefficient()) * velocityDelta;
+            var movingVelocityDelta = new VectorProjections(contact.angle())
+                    .setNormal(movingVelocityDeltaMagnitude)
+                    .recoverVelocity();
+            velocity
+                    .setX(velocity.getX() + movingVelocityDelta.getX())
+                    .setY(velocity.getY() + movingVelocityDelta.getY());
+        }
+    }
+}
