4a6ab0de32e52ff629a0d455b909704dab0a2b9a
A. Dvorak <dvorak88@mail.ru>
2025-05-13 19:13:20 +0400

    fix: BodyAcceleration remove unused methods and implement toString

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/common/BodyAcceleration.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/common/BodyAcceleration.java
@@ -1,6 +1,5 @@
 package com.github.aadvorak.artilleryonline.battle.common;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -19,17 +18,9 @@ public class BodyAcceleration implements BodyVector {
 
     private double angle;
 
-    @JsonIgnore
-    public Acceleration getMovingAcceleration() {
-        return new Acceleration()
-                .setX(x)
-                .setY(y);
-    }
-
-    public BodyAcceleration setMovingAcceleration(Acceleration acceleration) {
-        x = acceleration.getX();
-        y = acceleration.getY();
-        return this;
+    @Override
+    public String toString() {
+        return String.format("(%.3f, %.3f, %.3f)", x, y, angle);
     }
 
     public static BodyAcceleration sumOf(BodyAcceleration... accelerations) {
