886e05b44f6e03f4ca407ce6683cdcad553c2933
A. Dvorak <dvorak88@mail.ru>
2025-05-16 15:50:35 +0400

    fix: BodyForceAcceleration extractMovingFromRotating for pairs of forces with opposite torques

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/calculator/BodyAccelerationCalculator.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/calculator/BodyAccelerationCalculator.java
@@ -4,6 +4,8 @@ import com.github.aadvorak.artilleryonline.battle.calculations.BodyCalculations;
 import com.github.aadvorak.artilleryonline.battle.calculations.BodyForce;
 import com.github.aadvorak.artilleryonline.battle.calculations.ForceCalculator;
 import com.github.aadvorak.artilleryonline.battle.common.BodyAcceleration;
+import com.github.aadvorak.artilleryonline.battle.common.Force;
+import com.github.aadvorak.artilleryonline.battle.common.Vector;
 import com.github.aadvorak.artilleryonline.battle.config.Config;
 import com.github.aadvorak.artilleryonline.battle.model.BattleModel;
 import com.github.aadvorak.artilleryonline.battle.model.BodyModel;
@@ -34,6 +36,7 @@ public class BodyAccelerationCalculator<
         List<BodyAcceleration> accelerations = new ArrayList<>();
         forceCalculators.forEach(forceCalculator ->
                 forces.addAll(forceCalculator.calculate(calculations, battleModel)));
+        extractMovingFromRotating(forces);
         forces.forEach(force -> accelerations.add(toAcceleration(force, calculations)));
         addColliderAcceleration(accelerations, calculations);
         return BodyAcceleration.sumOf(accelerations);
@@ -52,6 +55,55 @@ public class BodyAccelerationCalculator<
             acceleration.setAngle(force.torque() / momentOfInertia);
         }
         return acceleration;
+    }
+
+    private void extractMovingFromRotating(List<BodyForce> forces) {
+        var extractedForces = new ArrayList<BodyForce>();
+        for (var i = 0; i < forces.size(); i++) {
+            BodyForce force1 = forces.get(i);
+            for (var j = i + 1; j < forces.size(); j++) {
+                BodyForce force2 = forces.get(j);
+                if (force1.rotating() != null && force2.rotating() != null) {
+                    var extractedForce = extractMovingFromRotating(force1, force2);
+                    if (extractedForce != null) {
+                        extractedForces.add(extractedForce);
+                    }
+                }
+            }
+        }
+        forces.addAll(extractedForces);
+        if (!extractedForces.isEmpty()) {
+            System.out.println(extractedForces.size() + " forces extracted");
+        }
+    }
+
+    private BodyForce extractMovingFromRotating(BodyForce force1, BodyForce force2) {
+        var torque1 = force1.torque();
+        var torque2 = force2.torque();
+        if (torque1 * torque2 >= 0) {
+            return null;
+        }
+        var absTorque1 = Math.abs(torque1);
+        var absTorque2 = Math.abs(torque2);
+        if (absTorque1 > absTorque2) {
+            return extractMovingFromRotating(force1, force2, absTorque1, absTorque2);
+        } else {
+            return extractMovingFromRotating(force2, force1, absTorque2, absTorque1);
+        }
+    }
+
+    private BodyForce extractMovingFromRotating(BodyForce greater, BodyForce lower,
+                                                 double greaterTorque, double lowerTorque) {
+        var diff = greaterTorque - lowerTorque;
+        var rotatingPart = diff / greaterTorque;
+        var movingPart = (greaterTorque - diff) / greaterTorque;
+        var newRotating = Force.of(greater.rotating().multiply(rotatingPart));
+        var movingFromGreater = Force.of(greater.rotating().multiply(movingPart));
+        var movingFromLower = lower.rotating();
+        greater.setRotating(newRotating);
+        lower.setRotating(null);
+        return BodyForce.atCOM(Force.of(Vector.sumOf(movingFromGreater, movingFromLower)),
+                "Extracted from " + greater.description() + " and " + lower.description());
     }
 
     private void addColliderAcceleration(List<BodyAcceleration> accelerations, C calculations) {
