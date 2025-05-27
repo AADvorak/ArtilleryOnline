fc5c41a3cc82b491494bd2cc8ec6c6b3481c2ff1
A. Dvorak <dvorak88@mail.ru>
2025-05-27 16:01:50 +0400

    fix: VehicleReturnOnGroundProcessor index out of range exception

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/VehicleReturnOnGroundProcessor.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/vehicle/VehicleReturnOnGroundProcessor.java
@@ -12,6 +12,16 @@ public class VehicleReturnOnGroundProcessor {
                 vehicle.getModel().setTurnedOverTime(currentTime);
             } else if (currentTime - vehicle.getModel().getTurnedOverTime()
                     >= vehicle.getModel().getSpecs().getTrackRepairTime() * 1000 && vehicle.getCollisions().isEmpty()) {
+                var position = vehicle.getModel().getState().getPosition();
+                var minX = battle.getModel().getRoom().getSpecs().getLeftBottom().getX();
+                var maxX = battle.getModel().getRoom().getSpecs().getRightTop().getX();
+                var maxRadius = vehicle.getModel().getPreCalc().getMaxRadius();
+                if (position.getX() - maxRadius < minX) {
+                    position.setX(minX + maxRadius);
+                }
+                if (position.getX() + maxRadius > maxX) {
+                    position.setX(maxX - maxRadius);
+                }
                 VehicleOnGroundProcessor.estimateVehicleAngleByPosition(vehicle.getModel(), battle.getModel().getRoom());
                 VehicleOnGroundProcessor.correctVehiclePositionAndAngleOnGround(vehicle.getModel(), battle.getModel().getRoom());
                 vehicle.getModel().getState().getGunState().setTargetAngle(
