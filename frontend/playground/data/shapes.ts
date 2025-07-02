13c1167f087c4c44e9c31bf24b29f441f1f7d71a
A. Dvorak <dvorak88@mail.ru>
2025-07-02 13:31:04 +0400

    feat: shapes.ts add Circle

--- frontend/playground/data/shapes.ts
+++ frontend/playground/data/shapes.ts
@@ -1,10 +1,15 @@
 export const ShapeNames = {
+  CIRCLE: 'Circle',
   HALF_CIRCLE: 'HalfCircle',
   TRAPEZE: 'Trapeze'
 }
 
 export interface Shape {
   name: string
+}
+
+export interface CircleShape extends Shape {
+  radius: number
 }
 
 export interface HalfCircleShape extends Shape {
