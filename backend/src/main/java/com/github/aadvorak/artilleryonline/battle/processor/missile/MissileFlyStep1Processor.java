e653a9b61a859e1918fdf2f38df4af794fa88dea
A. Dvorak <dvorak88@mail.ru>
2025-07-18 11:05:21 +0400

    fix: MissileFlyStep1Processor, MissileFlyStep2Processor

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/missile/MissileFlyStep1Processor.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/missile/MissileFlyStep1Processor.java
@@ -1,6 +1,9 @@
 package com.github.aadvorak.artilleryonline.battle.processor.missile;
 
 import com.github.aadvorak.artilleryonline.battle.calculations.BattleCalculations;
+import com.github.aadvorak.artilleryonline.battle.calculations.MissileCalculations;
+import com.github.aadvorak.artilleryonline.battle.calculator.MissileAccelerationCalculator;
+import com.github.aadvorak.artilleryonline.battle.model.BattleModel;
 import com.github.aadvorak.artilleryonline.battle.processor.Step1Processor;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +12,18 @@ public class MissileFlyStep1Processor implements Step1Processor {
 
     @Override
     public void process(BattleCalculations battle) {
-        battle.getMissiles().forEach(missile ->
-                missile.calculateNextPosition(battle.getModel().getCurrentTimeStepSecs()));
+        battle.getMissiles().forEach(missile -> processMissile(missile, battle));
+    }
+
+    private void processMissile(MissileCalculations missile, BattleCalculations battle) {
+        recalculateVelocity(missile, battle.getModel());
+        missile.calculateNextPosition(battle.getModel().getCurrentTimeStepSecs());
+    }
+
+    private void recalculateVelocity(MissileCalculations missile, BattleModel battleModel) {
+        var acceleration = MissileAccelerationCalculator.calculate(missile, battleModel);
+        var velocity = missile.getModel().getState().getVelocity();
+        var timeStep = battleModel.getCurrentTimeStepSecs();
+        velocity.recalculate(acceleration, timeStep);
     }
 }
--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/missile/MissileFlyStep2Processor.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/processor/missile/MissileFlyStep2Processor.java
@@ -2,8 +2,6 @@ package com.github.aadvorak.artilleryonline.battle.processor.missile;
 
 import com.github.aadvorak.artilleryonline.battle.calculations.BattleCalculations;
 import com.github.aadvorak.artilleryonline.battle.calculations.MissileCalculations;
-import com.github.aadvorak.artilleryonline.battle.calculator.MissileAccelerationCalculator;
-import com.github.aadvorak.artilleryonline.battle.model.BattleModel;
 import com.github.aadvorak.artilleryonline.battle.processor.Step2Processor;
 import org.springframework.stereotype.Component;
 
@@ -12,18 +10,6 @@ public class MissileFlyStep2Processor implements Step2Processor {
 
     @Override
     public void process(BattleCalculations battle) {
-        battle.getMissiles().forEach(missile -> processMissile(missile, battle.getModel()));
-    }
-
-    private void processMissile(MissileCalculations missile, BattleModel battleModel) {
-        missile.applyNextPosition();
-        recalculateVelocity(missile, battleModel);
-    }
-
-    private void recalculateVelocity(MissileCalculations missile, BattleModel battleModel) {
-        var acceleration = MissileAccelerationCalculator.calculate(missile, battleModel);
-        var velocity = missile.getModel().getState().getVelocity();
-        var timeStep = battleModel.getCurrentTimeStepSecs();
-        velocity.recalculate(acceleration, timeStep);
+        battle.getMissiles().forEach(MissileCalculations::applyNextPosition);
     }
 }
