c08d0b2d4e226af86a5dc37a0c4b9b1b62b1deea
A. Dvorak <dvorak88@mail.ru>
2025-09-05 09:42:27 +0400

    feat: BoxBoxCollisionsDetector

---
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/collision/detector/box/BoxBoxCollisionsDetector.java
@@ -0,0 +1,53 @@
+package com.github.aadvorak.artilleryonline.battle.collision.detector.box;
+
+import com.github.aadvorak.artilleryonline.battle.calculations.BattleCalculations;
+import com.github.aadvorak.artilleryonline.battle.calculations.BoxCalculations;
+import com.github.aadvorak.artilleryonline.battle.calculations.Calculations;
+import com.github.aadvorak.artilleryonline.battle.collision.Collision;
+import com.github.aadvorak.artilleryonline.battle.collision.detector.CollisionsDetector;
+import com.github.aadvorak.artilleryonline.battle.common.lines.BodyPart;
+import com.github.aadvorak.artilleryonline.battle.utils.CollisionUtils;
+import com.github.aadvorak.artilleryonline.battle.utils.ContactUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class BoxBoxCollisionsDetector implements CollisionsDetector {
+
+    @Override
+    public Set<Collision> detect(Calculations<?> calculations, BattleCalculations battle, boolean first) {
+        if (calculations instanceof BoxCalculations boxCalculations) {
+            return detect(boxCalculations, battle, first);
+        }
+        return Set.of();
+    }
+
+    private Set<Collision> detect(BoxCalculations box, BattleCalculations battle, boolean first) {
+        Set<Collision> collisions = new HashSet<>();
+        var otherBoxes = battle.getBoxes().stream()
+                .filter(value -> !Objects.equals(value.getId(), box.getId()))
+                .filter(value -> CollisionUtils.collisionNotDetected(box, value))
+                .collect(Collectors.toSet());
+        var maxRadius = box.getModel().getPreCalc().getMaxRadius();
+        var position = box.getGeometryNextPosition();
+        var bodyPart = BodyPart.of(position, box.getModel().getSpecs().getShape());
+        for (var otherBox : otherBoxes) {
+            var otherMaxRadius = otherBox.getModel().getPreCalc().getMaxRadius();
+            var otherPosition = otherBox.getGeometryNextPosition();
+            if (otherPosition.getCenter().distanceTo(position.getCenter()) > maxRadius + otherMaxRadius) {
+                continue;
+            }
+            var otherBodyPart = BodyPart.of(otherPosition, otherBox.getModel().getSpecs().getShape());
+            var contact = ContactUtils.getBodyPartsContact(bodyPart, otherBodyPart);
+            if (contact != null) {
+                collisions.add(Collision.withBox(box, otherBox, contact));
+                if (first) return collisions;
+            }
+        }
+        return collisions;
+    }
+}
