cb46219586072aa0ea65dc1c883662f97344c9be
A. Dvorak <dvorak88@mail.ru>
2025-11-18 09:41:10 +0400

    fix: JetSpecsPreset up

--- backend/src/main/java/com/github/aadvorak/artilleryonline/battle/preset/JetSpecsPreset.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/battle/preset/JetSpecsPreset.java
@@ -12,24 +12,24 @@ public enum JetSpecsPreset {
     LIGHT("light", new JetSpecs()
             .setCapacity(45.0)
             .setConsumption(15.0)
-            .setRegeneration(1.0)
-            .setAcceleration(18.0)
+            .setRegeneration(2.0)
+            .setAcceleration(20.0)
             .setType(JetType.VERTICAL)
     ),
 
     MEDIUM("medium", new JetSpecs()
             .setCapacity(45.0)
             .setConsumption(20.0)
-            .setRegeneration(1.0)
-            .setAcceleration(12.0)
+            .setRegeneration(1.5)
+            .setAcceleration(14.0)
             .setType(JetType.HORIZONTAL)
     ),
 
     HEAVY("heavy", new JetSpecs()
             .setCapacity(45.0)
             .setConsumption(20.0)
-            .setRegeneration(1.0)
-            .setAcceleration(10.0)
+            .setRegeneration(1.5)
+            .setAcceleration(12.0)
             .setType(JetType.HORIZONTAL)
     );
 
