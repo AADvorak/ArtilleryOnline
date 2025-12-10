1923c2dc7aae268e97bc931f81972d18ed036745
A. Dvorak <dvorak88@mail.ru>
2025-12-10 09:18:14 +0400

    test: SurfaceContactUtilsTest trapeze

--- backend/src/test/java/com/github/aadvorak/artilleryonline/SurfaceContactUtilsTest.java
+++ backend/src/test/java/com/github/aadvorak/artilleryonline/SurfaceContactUtilsTest.java
@@ -5,7 +5,9 @@ import com.github.aadvorak.artilleryonline.battle.common.Constants;
 import com.github.aadvorak.artilleryonline.battle.common.Position;
 import com.github.aadvorak.artilleryonline.battle.common.lines.Circle;
 import com.github.aadvorak.artilleryonline.battle.common.lines.HalfCircle;
+import com.github.aadvorak.artilleryonline.battle.common.lines.Trapeze;
 import com.github.aadvorak.artilleryonline.battle.common.shapes.CircleShape;
+import com.github.aadvorak.artilleryonline.battle.common.shapes.TrapezeShape;
 import com.github.aadvorak.artilleryonline.battle.state.SurfaceState;
 import com.github.aadvorak.artilleryonline.battle.utils.SurfaceContactUtils;
 import org.junit.jupiter.api.Test;
@@ -30,6 +32,8 @@ public class SurfaceContactUtilsTest {
     private static final Circle CIRCLE = Circle.of(BODY_POSITION, new CircleShape().setRadius(RADIUS));
 
     private static final HalfCircle HALF_CIRCLE = HalfCircle.of(BODY_POSITION, RADIUS);
+
+    private static final Trapeze TRAPEZE = new Trapeze(BODY_POSITION, TrapezeShape.rectangle(2 * RADIUS, RADIUS));
 
     @Test
     public void circleBottomContactExists() {
@@ -524,6 +528,254 @@ public class SurfaceContactUtilsTest {
                 .setWidth(SURFACE_WIDTH);
         roomModel.getState().setSurfaces(List.of(surface));
         var contacts = SurfaceContactUtils.getContacts(HALF_CIRCLE, roomModel, true);
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void trapezeBottomContactExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y - EDGE_DISTANCE + SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(0.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(-1.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeBottomContactNotExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y - EDGE_DISTANCE - SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void trapezeBottomContactExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y - EDGE_DISTANCE + SMALL_DELTA + roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(0.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(-1.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeBottomContactNotExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y - EDGE_DISTANCE - SMALL_DELTA + roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void trapezeTopContactExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y + EDGE_CIRCLE_DISTANCE - SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(0.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(1.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeTopContactNotExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y + EDGE_CIRCLE_DISTANCE + SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void trapezeTopContactExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y + EDGE_CIRCLE_DISTANCE - SMALL_DELTA - roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(0.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(1.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeTopContactNotExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceY = BODY_Y + EDGE_CIRCLE_DISTANCE + SMALL_DELTA - roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(BODY_X - 2 * RADIUS).setY(surfaceY))
+                .setEnd(new Position().setX(BODY_X + 2 * RADIUS).setY(surfaceY))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void trapezeRightContactExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X + EDGE_CIRCLE_DISTANCE - SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(1.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(0.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeRightContactNotExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X + EDGE_CIRCLE_DISTANCE + SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void trapezeRightContactExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X + EDGE_CIRCLE_DISTANCE - SMALL_DELTA - roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(1.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(0.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeRightContactNotExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X + EDGE_CIRCLE_DISTANCE + SMALL_DELTA - roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
+        assertTrue(contacts.isEmpty());
+    }
+
+@Test
+    public void trapezeLeftContactExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X - EDGE_CIRCLE_DISTANCE + SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(-1.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(0.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeLeftContactNotExists() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X - EDGE_CIRCLE_DISTANCE - SMALL_DELTA;
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, false);
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void trapezeLeftContactExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X - EDGE_CIRCLE_DISTANCE + SMALL_DELTA + roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
+        assertEquals(1, contacts.size());
+        var contact = contacts.stream().findAny().get();
+        assertAll(
+                () -> assertEquals(-1.0, contact.normal().getX(), SMALL_DELTA),
+                () -> assertEquals(0.0, contact.normal().getY(), SMALL_DELTA)
+        );
+    }
+
+    @Test
+    public void trapezeLeftContactNotExistsWithMaxDepth() {
+        var roomModel = TestRoomGenerator.generate();
+        var surfaceX = BODY_X - EDGE_CIRCLE_DISTANCE - SMALL_DELTA + roomModel.getSpecs().getGroundMaxDepth();
+        var surface = new SurfaceState()
+                .setBegin(new Position().setX(surfaceX).setY(BODY_Y - 2 * RADIUS))
+                .setEnd(new Position().setX(surfaceX).setY(BODY_Y + 2 * RADIUS))
+                .setWidth(SURFACE_WIDTH);
+        roomModel.getState().setSurfaces(List.of(surface));
+        var contacts = SurfaceContactUtils.getContacts(TRAPEZE, roomModel, true);
         assertTrue(contacts.isEmpty());
     }
 }
