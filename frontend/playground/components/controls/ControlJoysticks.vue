2a7887d8d439daf6dad99d34499cee8d779f26b1
A. Dvorak <dvorak88@mail.ru>
2025-12-17 11:15:55 +0400

    feat: ControlJoysticks icons

--- frontend/playground/components/controls/ControlJoysticks.vue
+++ frontend/playground/components/controls/ControlJoysticks.vue
@@ -10,6 +10,14 @@ import {useGlobalStateStore} from "~/stores/global-state";
 import {VerticalTooltipLocation} from "~/data/model";
 import VerticalTooltip from "~/components/vertical-tooltip.vue";
 import {useI18n} from "vue-i18n";
+import {
+  mdiTarget,
+  mdiTurbine,
+  mdiArrowLeftCircle,
+  mdiArrowRightCircle,
+  mdiArrowLeftBox,
+  mdiArrowRightBox
+} from "@mdi/js";
 
 const THRESHOLD = 0.3
 
@@ -154,6 +162,19 @@ onUnmounted(() => {
 
 <template>
   <div id="left-joystick-div" :class="leftJoystickClass">
+    <v-icon
+        class="icon-micro icon-top"
+        color="warning"
+        :icon="mdiTurbine"
+    />
+    <v-icon
+        class="icon-micro icon-left"
+        :icon="mdiArrowLeftBox"
+    />
+    <v-icon
+        class="icon-micro icon-right"
+        :icon="mdiArrowRightBox"
+    />
     <vertical-tooltip
         :tooltip="'< ' + t('controls.move') + ' >'"
         :show="showTooltip"
@@ -165,6 +186,19 @@ onUnmounted(() => {
     />
   </div>
   <div id="right-joystick-div" :class="rightJoystickClass">
+    <v-icon
+        class="icon-micro icon-top"
+        color="error"
+        :icon="mdiTarget"
+    />
+    <v-icon
+        class="icon-micro icon-left"
+        :icon="mdiArrowLeftCircle"
+    />
+    <v-icon
+        class="icon-micro icon-right"
+        :icon="mdiArrowRightCircle"
+    />
     <vertical-tooltip
         :tooltip="'< ' + t('controls.rotateGun') + ' >'"
         :show="showTooltip"
@@ -179,8 +213,8 @@ onUnmounted(() => {
 
 <style scoped>
 .joystick {
-  width: 85px;
-  height: 85px;
+  width: 84px;
+  height: 84px;
 }
 
 .bottom-left-joystick {
@@ -205,5 +239,26 @@ onUnmounted(() => {
   position: fixed;
   bottom: 50%;
   right: 8px;
+}
+
+.icon-micro {
+  position: absolute;
+  width: 14px;
+  height: 14px;
+}
+
+.icon-top {
+  top: 4px;
+  left: 31px;
+}
+
+.icon-right {
+  top: 35px;
+  right: 0;
+}
+
+.icon-left {
+  top: 35px;
+  left: 0;
 }
 </style>
