4eefcac0b10de218eda0bc3325060d0cd0a76387
A. Dvorak <dvorak88@mail.ru>
2026-02-12 15:50:47 +0400

    fix: EditUserRequest length

--- backend/src/main/java/com/github/aadvorak/artilleryonline/dto/request/EditUserRequest.java
+++ backend/src/main/java/com/github/aadvorak/artilleryonline/dto/request/EditUserRequest.java
@@ -12,10 +12,10 @@ public class EditUserRequest {
 
     @Email
     @NotEmpty
-    @Size(min = 5, max = 20)
+    @Size(min = 3, max = 20)
     private String email;
 
     @NotEmpty
-    @Size(min = 5, max = 20)
+    @Size(min = 2, max = 20)
     private String nickname;
 }
